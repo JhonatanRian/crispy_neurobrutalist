c2780e839e1f9213d1b5f93bd5b3063095df53af
Jhonatan Rian <64628254+JhonatanRian@users.noreply.github.com>
2025-06-29 12:20:56 -0300

    add initial HTML templates for form fields and layout

---
+++ src/crispy_neurobrutalist/layout.py
@@ -0,0 +1,41 @@
+from typing import Literal
+
+from crispy_forms.layout import BaseInput
+
+
+class Submit(BaseInput):
+    input_type = "submit"
+
+    def __init__(self, *args, css_class=None, **kwargs):
+        if css_class is None:
+            self.field_classes = "w-full font-bold text-lg text-white bg-black border-2 border-black rounded-lg py-3 neo-shadow-sm neo-button hover:bg-gray-800"
+        else:
+            self.field_classes = css_class
+        super().__init__(*args, **kwargs)
+
+
+class Button(BaseInput):
+    input_type = "button"
+
+    def __init__(
+        self,
+        *args,
+        css_class=None,
+        color: Literal["primary", "success", "warning", "danger", "purple"] = "primary",
+        **kwargs,
+    ):
+        if css_class is None:
+            mapcolor = {
+                "primary": "bg-blue-400 hover:bg-blue-500",
+                "success": "bg-green-400 hover:bg-green-500",
+                "warning": "bg-yellow-400 hover:bg-yellow-500",
+                "danger": "bg-red-400 hover:bg-red-500",
+                "purple": "bg-purple-400 hover:bg-purple-500",
+            }
+            self.field_classes = (
+                f"font-bold text-black {mapcolor[color]} border-2 border-black rounded-lg px-7 py-3 neo-shadow neo-button "
+                "transition-all"
+            )
+        else:
+            self.field_classes = css_class
+        super().__init__(*args, **kwargs)
